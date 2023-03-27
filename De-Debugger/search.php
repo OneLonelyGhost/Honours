2f4cc64a69e2f869448163ae487e253c72d7af7c
Charlie <91139038+OneLonelyGhost@users.noreply.github.com>
2023-03-27 16:26:10 +0100

    legal and footer updates

--- De-Debugger/search.php
+++ De-Debugger/search.php
@@ -10,8 +10,8 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
     <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.bundle.min.js.map"></script>
     <script type="text/javascript" src="js/search.js"></script>
@@ -225,7 +225,7 @@
             <a href="index.html" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
                 <img class="bi me-2" width="100%" height="100%" role="img" aria-label="Logo" src="img/Logo.png"></img>
             </a>
-            <p class="text-muted">&copy; 2022</p>
+            <p class="text-muted">&copy; 2023</p>
         </div>
 
         <div class="col mb-3">
@@ -244,43 +244,35 @@
         </div>
 
         <div class="col mb-3">
-            <h5>Policy</h5>
+            <h5>Legal</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Legal</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Privacy</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Cookies</a></li>
+                <li class="nav-item mb-2"><a href="privacy.html" class="nav-link p-0 text-muted">Privacy</a></li>
+                <li class="nav-item mb-2"><a href="cookies.html" class="nav-link p-0 text-muted">Cookies</a></li>
 
 
             </ul>
         </div>
 
         <div class="col mb-3 ">
-            <h5>Socials</h5>
-            <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Twitter</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Instagram</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Reddit</a></li>
 
-
-            </ul>
         </div>
     </footer>
     <script>
-        const form = document.querySelector('form');
-        const input = document.querySelector('input[type="text"]');
-        const previousSearchDiv = document.getElementById('previous-search');
-        const previousSearchTerm = localStorage.getItem('previousSearch');
+    const form = document.querySelector('form');
+    const input = document.querySelector('input[type="text"]');
+    const previousSearchDiv = document.getElementById('previous-search');
+    const previousSearchTerm = localStorage.getItem('previousSearch');
 
-        if (previousSearchTerm) {
-            previousSearchDiv.innerHTML = `${previousSearchTerm}`;
-        }
+    if (previousSearchTerm) {
+        previousSearchDiv.innerHTML = `${previousSearchTerm}`;
+    }
 
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            const searchTerm = input.value;
-            localStorage.setItem('previousSearch', searchTerm);
-            form.submit();
-        });
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const searchTerm = input.value;
+        localStorage.setItem('previousSearch', searchTerm);
+        form.submit();
+    });
     </script>
 </body>
 
